76a306f1d27ab503239c65222040b0283d0b8559
aluong15 <148726977+aluong15@users.noreply.github.com>
2024-10-03 22:47:09 -0500

    sql_data_exploration portfolio project 1 start

---
+++ importCsv.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from mysqlconnector import create_connection
+
+# Load CSV file using pandas
+csv_file_path = 'CovidDeaths.csv'
+df = pd.read_csv(csv_file_path)
+
+# For reference, inspect data types
+# print("Data Types of Each Column in file: ")
+# print(df.dtypes)
+
+df.columns = df.columns.str.strip()
+df.replace({'\n': '', '\r': ''}, regex=True, inplace=True)
+
+# Connect to DB
+connection = create_connection()
+cursor = connection.cursor()
+
+# Map pandas dtypes to MySQL data types
+def map_dtype_to_mysql(dtype):
+    match str(dtype):
+        case 'object':
+            return 'VARCHAR(50)'
+        case 'int64':
+            return 'INT'
+        case 'float64':
+            return 'FLOAT'
+        case 'bool':
+            return 'BOOLEAN'
+        case 'datetime64[ns]':
+            return 'DATETIME'
+        case _:
+            return 'TEXT'
+
+# Drop table if it exists
+table_name = 'coviddeaths'
+drop_table_query = f"DROP TABLE IF EXISTS {table_name};"
+cursor.execute(drop_table_query)
+print(f"Table {table_name} dropped if existed")
+
+# Generate column definitions based on df dtype
+columns = ', '.join([f"{col} {map_dtype_to_mysql(dtype)}" for col, dtype in df.dtypes.items()])
+# print(columns)
+
+# Create table query
+create_table_query = f"CREATE TABLE {table_name} ({columns});"
+print(f"Created table {table_name}")
+cursor.execute(create_table_query)
+
+# Insert data into MySQL table
+# Convert df to list of tuples to insert
+for i, row in df.iterrows():
+    row_values = ', '.join(['%s'] * len(row))
+    insert_query = f"INSERT INTO {table_name} VALUES ({row_values})"
+    cursor.execute(insert_query, tuple(row))
+
+# Commit the changes and close the connection
+connection.commit()
+cursor.close()
+connection.close()
+print("Connection closed.")
+
+print(f"Data from {csv_file_path} has been successfully imported into MySQL table '{table_name}'")
